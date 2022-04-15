946049d1b0bdfcd56baeaea578b2d6555c540dd2
Samandar Qodirqulov <samandarqodirqulov021@gmail.com>
2022-04-15 17:07:30 +0500

    s

--- src/js/view/recipeView.js
+++ src/js/view/recipeView.js
@@ -1,159 +1,159 @@
-import icons from '../../img/icons.svg'; // Parcel 1-versiya
-// import icons from 'url:../img/icons.svg'; // Parcel 2- versiya
+// import icons from '../../img/icons.svg'; // Parcel 1-versiya
+// // import icons from 'url:../img/icons.svg'; // Parcel 2- versiya
 
-class RecipeView {
-  #errorMessage = 'Siz qidirayotgan amlumot topladi';
-  #parentElement = document.querySelector('.recipe');
-  #data;
+// class RecipeView {
+//   #errorMessage = 'Siz qidirayotgan amlumot topladi';
+//   #parentElement = document.querySelector('.recipe');
+//   #data;
 
-  //   // Moduldan kelgan malumot controlda osha malumotni borib olib keladigan fungsiya
+//   //   // Moduldan kelgan malumot controlda osha malumotni borib olib keladigan fungsiya
 
-  render(data) {
-    //bu yerda data moduldan kelgan statening resipe obj isiga teng
-    this.#data = data; //olib kelingan malumotlarni globalni qilib olgan uzgaruvchimizga uzlashtirib oldik
+//   render(data) {
+//     //bu yerda data moduldan kelgan statening resipe obj isiga teng
+//     this.#data = data; //olib kelingan malumotlarni globalni qilib olgan uzgaruvchimizga uzlashtirib oldik
 
-    if (!data) return;
+//     if (!data) return;
 
-    this.#clearHtml();
-    this.#generateHTML();
-  }
+//     this.#clearHtml();
+//     this.#generateHTML();
+//   }
 
-  //   renderError() {
-  //     const html = `
-  //      <div class="error">
-  //             <div>
-  //               <svg>
-  //                 <use href="${icons}#icon-alert-triangle"></use>
-  //               </svg>
-  //             </div>
-  //             <p>${this.#errorMessage}</p>
-  //      </div>
-  //   `;
-  //     this.#clearHtml();
-  //     this.#parentElement.insertAdjacentHTML('afterbegin', html);
-  //   }
+//   //   renderError() {
+//   //     const html = `
+//   //      <div class="error">
+//   //             <div>
+//   //               <svg>
+//   //                 <use href="${icons}#icon-alert-triangle"></use>
+//   //               </svg>
+//   //             </div>
+//   //             <p>${this.#errorMessage}</p>
+//   //      </div>
+//   //   `;
+//   //     this.#clearHtml();
+//   //     this.#parentElement.insertAdjacentHTML('afterbegin', html);
+//   //   }
 
-  //   #renderIng(ings) {
-  //     return ings.map(
-  //       val => `<li class="recipe__ingredient">
-  //     <svg class="recipe__icon">
-  //       <use href="${icons}#icon-check"></use>
-  //     </svg>
-  //     <div class="recipe__quantity">${val.quantity || ''}</div>
-  //     <div class="recipe__description">
-  //       <span class="recipe__unit">${val.unit}</span>
-  //       ${val.description}
-  //     </div>
-  //   </li>`
-  //     );
-  //   }
+//   //   #renderIng(ings) {
+//   //     return ings.map(
+//   //       val => `<li class="recipe__ingredient">
+//   //     <svg class="recipe__icon">
+//   //       <use href="${icons}#icon-check"></use>
+//   //     </svg>
+//   //     <div class="recipe__quantity">${val.quantity || ''}</div>
+//   //     <div class="recipe__description">
+//   //       <span class="recipe__unit">${val.unit}</span>
+//   //       ${val.description}
+//   //     </div>
+//   //   </li>`
+//   //     );
+//   //   }
 
-  #generateHTML() {
-    const html = `<figure class="recipe__fig">
-    <img src="${this.#data.image}" alt="Tomato" class="recipe__img" />
-    <h1 class="recipe__title">
-      <spanthis.#data.title}</span>
-    </h1>
-  </figure>
+//   #generateHTML() {
+//     const html = `<figure class="recipe__fig">
+//     <img src="${this.#data.image}" alt="Tomato" class="recipe__img" />
+//     <h1 class="recipe__title">
+//       <spanthis.#data.title}</span>
+//     </h1>
+//   </figure>
 
-  <div class="recipe__details">
-    <div class="recipe__info">
-      <svg class="recipe__info-icon">
-        <use href="${icons}#icon-clock"></use>
+//   <div class="recipe__details">
+//     <div class="recipe__info">
+//       <svg class="recipe__info-icon">
+//         <use href="${icons}#icon-clock"></use>
+// //       </svg>
+// //       <span class="recipe__inthis.#data recipe__info-data--minutes">${
+//       this.#data.time
+//     }</span>
+//       <span class="recipe__info-text">minutes</span>
+//     </div>
+//     <div class="recipe__info">
+//       <svg class="recipe__info-icon">
+//         <use href="${icons}#icon-users"></use>
 //       </svg>
-//       <span class="recipe__inthis.#data recipe__info-data--minutes">${
-      this.#data.time
-    }</span>
-      <span class="recipe__info-text">minutes</span>
-    </div>
-    <div class="recipe__info">
-      <svg class="recipe__info-icon">
-        <use href="${icons}#icon-users"></use>
-      </svg>
-      <span class="recipe__info-data recipe__info-data--people">${
-        this.#data.servings
-      }</span>
-      <span class="recipe__info-text">servings</span>
+//       <span class="recipe__info-data recipe__info-data--people">${
+//         this.#data.servings
+//       }</span>
+//       <span class="recipe__info-text">servings</span>
 
-      <div class="recipe__info-buttons">
-        <button class="btn--tiny btn--increase-servings">
-          <svg>
-            <use href="${icons}#icon-minus-circle"></use>
-          </svg>
-        </button>
-        <button class="btn--tiny btn--increase-servings">
-          <svg>
-//             <use href="${icons}#icon-plus-circle"></use>
+//       <div class="recipe__info-buttons">
+//         <button class="btn--tiny btn--increase-servings">
+//           <svg>
+//             <use href="${icons}#icon-minus-circle"></use>
 //           </svg>
 //         </button>
-//       </div>
-//     </div>
+//         <button class="btn--tiny btn--increase-servings">
+//           <svg>
+// //             <use href="${icons}#icon-plus-circle"></use>
+// //           </svg>
+// //         </button>
+// //       </div>
+// //     </div>
 
-//     <div class="recipe__user-generated">
-//       <svg>
-//         <use href="${icons}#icon-user"></use>
+// //     <div class="recipe__user-generated">
+// //       <svg>
+// //         <use href="${icons}#icon-user"></use>
+// //       </svg>
+// //     </div>
+// //     <button class="btn--round">
+// //       <svg class="">
+// //         <use href="${icons}#icon-bookmark-fill"></use>
+// //       </svg>
+// //     </button>
+// //   </div>
+
+// //   <div class="recipe__ingredients">
+// //     <h2 class="heading--2">Recipe ingredients</h2>
+// //     <ul class="recipe__ingredient-list">
+// //     ${this.#renderIng(this.#data.ingredients).join('')}
+
+// //     </ul>
+// //   </div>
+
+// //   <div class="recipe__directions">
+// //     <h2 class="heading--2">How to cook it</h2>
+// //     <p class="recipe__directions-text">
+// //       This recipe was carefully designed and tested by
+// //       <span class="recipe__publisher">${
+//       this.#data.publisher
+//     }</span>. Please check out
+//       directions at their website.
+//     </p>
+//     <a
+//       class="btn--small recipe__btn"
+//       href="${this.#data.source_url}"
+//       target="_blank"
+//     >
+//       <span>Directions</span>
+//       <svg class="search__icon">
+//         <use href="${icons}#icon-arrow-right"></use>
 //       </svg>
-//     </div>
-//     <button class="btn--round">
-//       <svg class="">
-//         <use href="${icons}#icon-bookmark-fill"></use>
-//       </svg>
-//     </button>
-//   </div>
+//     </a>
+//   </div>`;
 
-//   <div class="recipe__ingredients">
-//     <h2 class="heading--2">Recipe ingredients</h2>
-//     <ul class="recipe__ingredient-list">
-//     ${this.#renderIng(this.#data.ingredients).join('')}
+//     this.#clearHtml();
+//     this.#parentElement.insertAdjacentHTML('afterbegin', html);
+//   }
 
-//     </ul>
-//   </div>
+//   #clearHtml() {
+//     this.#parentElement.innerHTML = '';
+//   }
 
-//   <div class="recipe__directions">
-//     <h2 class="heading--2">How to cook it</h2>
-//     <p class="recipe__directions-text">
-//       This recipe was carefully designed and tested by
-//       <span class="recipe__publisher">${
-      this.#data.publisher
-    }</span>. Please check out
-      directions at their website.
-    </p>
-    <a
-      class="btn--small recipe__btn"
-      href="${this.#data.source_url}"
-      target="_blank"
-    >
-      <span>Directions</span>
-      <svg class="search__icon">
-        <use href="${icons}#icon-arrow-right"></use>
-      </svg>
-    </a>
-  </div>`;
+//   //   loading() {
+//   //     const html = `
+//   //     <div class="spinner">
+//   //       <svg>
+//   //         <use href="${icons}#icon-loader"></use>
+//   //       </svg>
+//   //     </div> `;
 
-    this.#clearHtml();
-    this.#parentElement.insertAdjacentHTML('afterbegin', html);
-  }
+//   //     this.#clearHtml();
+//   //     this.#parentElement.insertAdjacentHTML('afterbegin', html);
+//   //   }
 
-  #clearHtml() {
-    this.#parentElement.innerHTML = '';
-  }
-
-  //   loading() {
-  //     const html = `
-  //     <div class="spinner">
-  //       <svg>
-  //         <use href="${icons}#icon-loader"></use>
-  //       </svg>
-  //     </div> `;
-
-  //     this.#clearHtml();
-  //     this.#parentElement.insertAdjacentHTML('afterbegin', html);
-  //   }
-
-  addHandlerEvent(data) {
-    ['hashchange', 'load'].forEach(val => window.addEventListener(val, data)); //bu yerda data showRecipega teng
-    // window.addEventListener('hashchange', showRecipe);
-    // window.addEventListener('load', showRecipe);
-  }
-}
-// export default new RecipeView(); //obj cha yaratib export qildik.Classlarni doim shunaqa qilib export qilishimiz kerak
+//   addHandlerEvent(data) {
+//     ['hashchange', 'load'].forEach(val => window.addEventListener(val, data)); //bu yerda data showRecipega teng
+//     // window.addEventListener('hashchange', showRecipe);
+//     // window.addEventListener('load', showRecipe);
+//   }
+// }
+// // export default new RecipeView(); //obj cha yaratib export qildik.Classlarni doim shunaqa qilib export qilishimiz kerak
